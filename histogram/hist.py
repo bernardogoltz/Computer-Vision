29188ff88e41dace84c45bedfe675a1552f41199
Bernardo <bergoltzx2@gmail.com>
2022-11-08 12:37:08 -0300

    Create hist.py

---
+++ histogram/hist.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  8 12:16:09 2022
+
+@author: bernardogoltz
+"""
+
+import cv2 as cv 
+import matplotlib.pyplot as plt 
+
+
+img = cv.imread('C:/Users/angel/OneDrive/Documentos/computer-vision/Computer-Vision/resources/images/Imagens/Cinza.jpg')
+
+plt.imshow(img)
+
